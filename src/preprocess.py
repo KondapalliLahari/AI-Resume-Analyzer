e1fca1b11a3d2fcbe707b5a5238b44098f282ead
Lahari Kondapalli <kondapallilahari07@gmail.com>
2026-06-08 15:03:57 +0530

    Fix NLTK resources

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,15 +2,14 @@ import re
 import nltk
 
 # Download required NLTK data on Streamlit Cloud
-try:
-    nltk.data.find("corpora/stopwords")
-except LookupError:
-    nltk.download("stopwords")
-
-try:
-    nltk.data.find("tokenizers/punkt")
-except LookupError:
-    nltk.download("punkt")
+for resource in ["stopwords", "punkt", "punkt_tab"]:
+    try:
+        nltk.data.find(
+            f"corpora/{resource}" if resource == "stopwords"
+            else f"tokenizers/{resource}"
+        )
+    except LookupError:
+        nltk.download(resource)
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
